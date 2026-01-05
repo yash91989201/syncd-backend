702c2a6f2e9b0229645388fa131c5e2101049b52
Yashraj Jaiswal <yashraj6370@gmail.com>
2026-01-05 19:57:52 +0530

    put delete procedure in user profile

--- packages/api/src/routers/user-profile.ts
+++ packages/api/src/routers/user-profile.ts
@@ -1,6 +1,8 @@
+import { auth } from "@syncd-backend/auth";
 import { userProfile } from "@syncd-backend/db/schema/user-profile";
 import { eq } from "drizzle-orm";
 import { protectedProcedure } from "../index";
+import { DeleteAccountOutput } from "../lib/schema/account";
 
 export const userProfileRouter = {
   get: protectedProcedure.handler(async ({ context: { session, db } }) => {
@@ -9,5 +11,11 @@ export const userProfileRouter = {
     });
 
     return userProfileData;
+  }),
+
+  delete: protectedProcedure.output(DeleteAccountOutput).handler(async () => {
+    const res = await auth.api.deleteUser();
+
+    return res;
   }),
 };
